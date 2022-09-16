ee534b3158ab868a10e4b99783f72c5fa30f4c22
dmiv <dmitriy40647274@gmail.com>
2022-09-16 22:22:18 +0300

    first commit

---
+++ vue/hello_nuxt/pages/fun.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <h1>Fun page</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: "fun"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/hello_nuxt/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  Hello, nuxt
+  link to <NuxtLink to="./fun">fun router</NuxtLink>
+</div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
