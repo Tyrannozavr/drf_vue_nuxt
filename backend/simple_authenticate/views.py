3b5c786f2832b2cbdbb074099e835c70e0a7c220
dmiv <dmitriy40647274@gmail.com>
2022-09-17 00:11:22 +0300

    authentication is work

--- backend/simple_authenticate/views.py
+++ backend/simple_authenticate/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.decorators import api_view, renderer_classes, permission_classes
 from rest_framework.renderers import JSONRenderer
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 
 @api_view(['GET'])
 @renderer_classes([JSONRenderer])
+@permission_classes([IsAuthenticated])
+# @permission_classes([AllowAny])
 def index(request):
     return Response(data={'Hello': 'hello'})
